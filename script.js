09b99c44deeb03fabc3554aeca5bc0545cc477e0
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-06-02 16:14:55 +0300

    Moving

--- script.js
+++ script.js
@@ -1,74 +1,20 @@
-"use strict";
-// Variables
-const car1 = document.querySelector(".car1");
-const car2 = document.querySelector(".car2");
-const timeInput1 = document.getElementById("time-input-car1");
-const timeInput2 = document.getElementById("time-input-car2");
-const submitButton = document.getElementById("submit-button");
-const result = document.getElementById("result");
-const reset = document.getElementById("reset-button");
-const cars = document.querySelector(".car");
-let winner;
-let time1;
-let time2;
-
-// Functions
-const resetAnimation = function () {
-  cars.style.animationFillMode = "";
-  car1.style.animationName = "";
-  car2.style.animationName = "";
-};
-
-const cancelSubmitButton = submitButton.addEventListener(
-  "click",
-  function (event) {
-    event.preventDefault();
+const btn = document.querySelector(".dark-mode");
+const root = document.documentElement;
+const home = document.querySelector(".home");
+let darkMode = true;
+btn.addEventListener("click", function () {
+  if (darkMode) {
+    root.style.setProperty("--textcolor", "#333333");
+    root.style.setProperty("--background", "#f2f2f2");
+    root.style.setProperty("--maincolor", "#007acc");
+    home.style.setProperty("background", "none");
+  } else {
+    root.style.setProperty("--textcolor", "#ffffff");
+    root.style.setProperty("--background", "#1f1f1f");
+    root.style.setProperty("--maincolor", "#ff6600");
+    home.style.setProperty("background", "url(background-landing-page.jpeg)");
+    home.style.setProperty("background-size", "cover");
+    home.style.setProperty("background-position", "center");
   }
-);
-
-const startRace = function () {
-  time1 = timeInput1.value;
-  time2 = timeInput2.value;
-
-  car1.style.animationDuration = `${time1}s`;
-  car2.style.animationDuration = `${time2}s`;
-  car1.style.animationName = "race1";
-  car2.style.animationName = "race2";
-  car1.style.display = "block";
-  car2.style.display = "block";
-  result.textContent = "Race started!";
-};
-
-// Start of code 
-submitButton.addEventListener("click", startRace);
-
-car1.addEventListener("animationend", function () {
-  if (Number(time1) < Number(time2)) {
-    winner = "Car 1";
-    result.textContent = `${winner} won the race!`;
-    submitButton.addEventListener("click", function () {
-      result.textContent = "Press reset";
-    });
-  }
-});
-
-car2.addEventListener("animationend", function () {
-  if (Number(time1) > Number(time2)) {
-    winner = "Car 2";
-    result.textContent = `${winner} won the race!`;
-    submitButton.addEventListener("click", function () {
-      result.textContent = "Press reset";
-    });
-  }
-});
-
-reset.addEventListener("click", function () {
-  // car1.style.display = "none";
-  // car2.style.display = "none";
-  // car1.style.classList.remove("animationName");
-  resetAnimation();
-  result.textContent = "Enter again!";
-  submitButton.addEventListener("click", function () {
-    result.textContent = "Race started!";
-  });
+  darkMode = !darkMode;
 });
